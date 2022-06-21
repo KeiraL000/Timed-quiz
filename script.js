7d68b7d7ac29e73f98b5368e06d61fb61b4f5831
KeiraL000 <keira5466@yahoo.com>
2022-06-21 16:04:52 -0700

    "final commit"

--- script.js
+++ script.js
@@ -181,6 +181,6 @@ var highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 highScoresList.innerHTML =
 highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
+    return `<li class="high-scores">${score.name} - ${score.score}</li>`
 })
 
